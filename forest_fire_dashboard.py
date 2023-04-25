8530bcfbbd391821c82b532e08c474f881ba92f3
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-04-24 22:50:09 -0400

    Updated file name

--- forest_fire_dashboard.py
+++ forest_fire_dashboard.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+
 import streamlit as st 
 
 ## Run this demo locally
