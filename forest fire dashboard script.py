d27871419d5748b5a61c670fcca9932c46db43fd
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-04-24 22:22:47 -0400

    Added streamlit run forest fire dashboard.py

--- forest fire dashboard script.py
+++ forest fire dashboard script.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
+streamlit run forest fire dashboard.py
 import streamlit as st 
 
 
