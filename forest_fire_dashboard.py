a67c9f248bb161076f88bc0fe1b29efc81105ba3
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-04-26 16:18:33 -0400

    import scikit-learn as sklearn

--- forest_fire_dashboard.py
+++ forest_fire_dashboard.py
@@ -1,4 +1,4 @@
-pip install scikit-learn
+import scikit-learn as sklearn
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 #import matplotlib as plt
