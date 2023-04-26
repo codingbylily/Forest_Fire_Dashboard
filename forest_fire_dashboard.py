20b1395831b08484dfd133df9eb15e4829249324
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-04-26 19:37:49 -0400

    Spelling fixes

--- forest_fire_dashboard.py
+++ forest_fire_dashboard.py
@@ -32,8 +32,7 @@ st.header('About the data behind the model')
 data
 
 #drawing a line chart for FFMC and temperature comparison within personal data
-chart_data = data['temp','FMCC']
-
+chart_data = data['temp','FFMC']
 st.line_chart(chart_data)
 
 # data.head()
