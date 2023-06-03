64a6f424ae59d8c93cdcafefbe21666ed0b92cfc
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-06-02 20:39:53 -0400

    Update forest_fire_dashboard.py
    updating sklearn syntax

--- forest_fire_dashboard.py
+++ forest_fire_dashboard.py
@@ -1,4 +1,4 @@
-import scikit-learn as sklearn
+import sklearn
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 #import matplotlib as plt
