2855bc0164dbbf41149c98678866699ee892387e
Lily A. S <76699414+codingbylily@users.noreply.github.com>
2023-06-05 02:37:10 -0400

    Add files via upload

--- forest_fire_dashboard.py
+++ forest_fire_dashboard.py
@@ -45,7 +45,7 @@ with open("model.pkl", "rb") as f:
 
 
 #getting user inputs
-st.sidebar.[Input Variables]
+st.sidebar.write[Input Variables]
 ISI = st.sidebar.slider('Initial Spread Index (ISI)', min_value=0,max_value=56,value=4)
 temp = st.sidebar.slider('Temperature (Celsius degrees)', min_value=2,max_value=33,value=4)
 wind = st.sidebar.slider('Wind Speed (km/hr)', min_value=1,max_value=9,value=4)
